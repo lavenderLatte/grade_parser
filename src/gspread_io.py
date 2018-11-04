5c5ef6498171e49f9b53e9f0a3fcb580c3992509
lavenderLatte <tohanara@gmail.com>
2018-11-03 18:02:57 -0700

    add unit test. handle existing worksheet/ create new worksheet better

--- src/gspread_io.py
+++ src/gspread_io.py
@@ -1,10 +1,14 @@
 #!/opt/anaconda/anaconda2/bin/python
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import sys
+import random
+import numpy as np
 
 class gspread_io:
   _scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
   def __init__(self, cred_fname, sp_name):
+    print 'cred_fname = %s, sp_name = %s' %(cred_fname, sp_name)
     credentials = ServiceAccountCredentials.from_json_keyfile_name(cred_fname, self._scope)
     self._gc = gspread.authorize(credentials)
     self._sp = self._gc.open(sp_name)
@@ -13,9 +17,6 @@ class gspread_io:
     if sheet_name == None:
       print "sheet_name = %s passed. Returning" % sheet_name
       return
-    if table == None:
-      print "table = %s passed. Returning" % table
-      return
     
     num_rows = len(table)
     num_cols = len(table[0])
@@ -23,11 +24,13 @@ class gspread_io:
       print 'only <= 26 columns are supported for now. sheet_name = %s, num_cols = %d' % (sheet_name, num_cols)
       return
     
-    sht = self._sp.worksheet(sheet_name)
-    if not sht:
-      print 'Adding new sheet %s' % (sheet_name)
-      sht = self._sp.add_worksheet(sheet_name, rows='100', cols='20')
-    
+    try:
+      sht = self._sp.worksheet(sheet_name)
+      print 'Update existing worksheet %s' % sheet_name
+    except:
+      sht =self._sp.add_worksheet(sheet_name, rows='100', cols='20')
+      print 'Add new worksheet %s' % sheet_name
+
     cell_range = '{col_i}{row_i}:{col_f}{row_f}'.format(
                   col_i=chr((1-1) + ord('A')),    # converts number to letter
                   col_f=chr((num_cols-1) + ord('A')),      # subtract 1 because of 0-indexing
@@ -39,11 +42,17 @@ class gspread_io:
     for cell in cells:
       cell.value = table[cell.row-1][cell.col-1]
     
-    sht.update_cells(cell)
+    sht.update_cells(cells)
 
     print "updated sheet %s" % sheet_name
 
-def main(cred_fname, sp_name):
+def main():
+  g = gspread_io(sys.argv[1], 'testproject_spreadsheet')
+
+  g.add_table('sheet2', np.random.randint(100, size=(3,4)))
   
+
+if __name__ == '__main__':
+  main()
 
 
